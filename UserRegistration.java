51a87b8fcb969a7fd38b6b242f4f3382e54c953e
unknown <aavik743@gmail.com>
2022-02-14 23:46:32 +0530

    added: regex to first name

---
+++ UserRegistration.java
@@ -0,0 +1,25 @@
+package com.bl.regex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter first name: ");
+		String firstName = sc.next();
+		boolean isFirstName = Pattern.matches("[A-Z]+[a-z]{3,}", firstName);
+		
+		if(isFirstName) {
+			System.out.println("Valid first name");
+		}	
+		else {
+			System.out.println("Invalid first name");
+		}
+
+	}
+
+}
+
